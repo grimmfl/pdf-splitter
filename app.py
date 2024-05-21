f656c534199e931fbb10008a062ef681d663924c
grimmfl <flx.grimm@gmail.com>
2024-05-21 11:55:50 +0200

    #4 target dir

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from tkinter.ttk import Button, Label, Progressbar, Frame, Entry
 from pdf_splitter import PdfSplitter
 
 FILEPATHS = []
+TARGET_DIR = None
 
 
 def browse_files_callback():
@@ -15,10 +16,19 @@ def browse_files_callback():
     callback function for opening the file explorer
     """
     global FILEPATHS
-    FILEPATHS = filedialog.askopenfilenames(initialdir="",
+    FILEPATHS = filedialog.askopenfilenames(initialdir="./",
                                             title="Select files",
                                             filetypes=(("PDFs", "*.pdf"),))
     files_label["text"] = f"{len(FILEPATHS)} files selected"
+
+
+def browse_target_dir_callback():
+    """
+    callback function for opening the directory explorer
+    """
+    global TARGET_DIR
+    TARGET_DIR = filedialog.askdirectory(initialdir="./")
+    target_dir_label["text"] = f"...{TARGET_DIR[-20:]}"
 
 
 def split_files_callback():
@@ -27,8 +37,8 @@ def split_files_callback():
     """
     suffix = suffix_input.get().strip()
 
-    target_dir = target_dir_input.get().strip()
-    target_dir = None if target_dir == "" else target_dir
+    target_dir = None if TARGET_DIR == "" else TARGET_DIR
+    print(TARGET_DIR)
 
     if len(FILEPATHS) == 0:
         messagebox.showerror("Error", "No files selected")
@@ -46,34 +56,36 @@ app = Tk()
 app.title("PDF Splitter")
 
 # set minimum window size value
-app.minsize(400, 400)
+app.minsize(500, 400)
 # set maximum window size value
-app.maxsize(400, 400)
+app.maxsize(500, 400)
 
 frame = Frame(app)
 
 explore_button = Button(frame, text="Browse Files", command=browse_files_callback)
-explore_button.grid(row=0, column=0, pady=5)
+explore_button.grid(row=0, column=0, pady=7, padx=7)
 
 files_label = Label(frame, text="")
-files_label.grid(row=0, column=1, pady=5)
+files_label.grid(row=0, column=1, pady=7, padx=7)
 
-Label(frame, text="Suffix").grid(row=1, column=0, pady=5)
+Label(frame, text="Suffix").grid(row=1, column=0, pady=7, padx=7)
 suffix_input = Entry(frame)
-suffix_input.grid(row=1, column=1, pady=5)
+suffix_input.grid(row=1, column=1, pady=7, padx=7)
 
-Label(frame, text="Target Directory").grid(row=2, column=0, pady=5)
-target_dir_input = Entry(frame)
-target_dir_input.grid(row=2, column=1, pady=5)
+target_dir_button = Button(frame, text="Select Target Directory", command=browse_target_dir_callback)
+target_dir_button.grid(row=2, column=0, pady=7, padx=7)
+
+target_dir_label = Label(frame, text="optional")
+target_dir_label.grid(row=2, column=1, pady=7, padx=7)
 
 split_button = Button(frame, text="Split Files", command=split_files_callback)
-split_button.grid(row=3, column=0, pady=5)
+split_button.grid(row=3, column=0, pady=7, padx=7)
 
 progress = Progressbar(frame)
-progress.grid(row=3, column=1, pady=5)
+progress.grid(row=3, column=1, pady=7, padx=7)
 
 close_button = Button(frame, text="Close", command=app.quit)
-close_button.grid(row=4, column=0, pady=5)
+close_button.grid(row=4, column=0, pady=7, padx=7)
 
 frame.place(anchor='center', relx=.5, rely=.45)
 
