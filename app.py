1e4bb0a9b88d43a2f47830f52220e0b4dd19d105
grimmfl <flx.grimm@gmail.com>
2024-05-21 11:27:50 +0200

    #4 target dir

--- app.py
+++ app.py
@@ -26,12 +26,16 @@ def split_files_callback():
     callback function handling click on split files
     """
     suffix = suffix_input.get().strip()
+
+    target_dir = target_dir_input.get().strip()
+    target_dir = None if target_dir == "" else target_dir
+
     if len(FILEPATHS) == 0:
         messagebox.showerror("Error", "No files selected")
         return
 
     for path in FILEPATHS:
-        splitter = PdfSplitter(path, suffix)
+        splitter = PdfSplitter(path, suffix, target_dir)
         splitter.split()
         progress.step(1 / len(FILEPATHS) * 100)
 
@@ -55,18 +59,21 @@ files_label = Label(frame, text="")
 files_label.grid(row=0, column=1, pady=5)
 
 Label(frame, text="Suffix").grid(row=1, column=0, pady=5)
-
 suffix_input = Entry(frame)
 suffix_input.grid(row=1, column=1, pady=5)
 
+Label(frame, text="Target Directory").grid(row=2, column=0, pady=5)
+target_dir_input = Entry(frame)
+target_dir_input.grid(row=2, column=1, pady=5)
+
 split_button = Button(frame, text="Split Files", command=split_files_callback)
-split_button.grid(row=2, column=0, pady=5)
+split_button.grid(row=3, column=0, pady=5)
 
 progress = Progressbar(frame)
-progress.grid(row=2, column=1, pady=5)
+progress.grid(row=3, column=1, pady=5)
 
 close_button = Button(frame, text="Close", command=app.quit)
-close_button.grid(row=3, column=0, pady=5)
+close_button.grid(row=4, column=0, pady=5)
 
 frame.place(anchor='center', relx=.5, rely=.45)
 
--- pdf_splitter.py
+++ pdf_splitter.py
@@ -8,18 +8,22 @@ from pypdf.generic import RectangleObject
 
 from exceptions import PaperSizeException
 
+import os
+
 
 class PdfSplitter:
     """
     PdfSplitter splits all DIN A3 pages of a pdf into DIN A3 pages.
     :param filepath: path to pdf file
     :param result_suffix: suffix of the resulting pdf file
+    :param target_dir: directory for the resulting pdf file
     """
-    def __init__(self, filepath: str, result_suffix: str = "_splitted"):
+    def __init__(self, filepath: str, result_suffix: str = "_splitted", target_dir=None):
         self._reader = PdfReader(filepath)
         self._writer = PdfWriter()
         self._path = filepath
         self._result_suffix = result_suffix
+        self._target_dir = target_dir
 
     def split(self):
         """
@@ -32,7 +36,7 @@ class PdfSplitter:
 
             self._write_single_pages(page)
 
-        with open(f"{self._path[:-4]}{self._result_suffix}.pdf", "wb") as file:
+        with open(self._target_path, "wb") as file:
             self._writer.write(file)
 
     def _write_single_pages(self, page: PageObject):
@@ -59,8 +63,23 @@ class PdfSplitter:
 
         raise PaperSizeException(width, height)
 
+    @property
+    def _target_path(self) -> str:
+        if self._target_dir is None:
+            return f"{self._path[:-4]}{self._result_suffix}.pdf"
+
+        if not os.path.isdir(self._target_dir):
+            os.mkdir(self._target_dir)
+
+        if not self._target_dir.endswith("/"):
+            self._target_dir += "/"
+
+        filename = self._path.split("/")[-1] if "/" in self._path else self._path
+
+        return f"{self._target_dir}{filename[:-4]}{self._result_suffix}.pdf"
+
 
 if __name__ == '__main__':
     path = sys.argv[1]
-    splitter = PdfSplitter(path)
+    splitter = PdfSplitter(path, "_test", "result")
     splitter.split()
